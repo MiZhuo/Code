7043a285f068f719f277bf88c85eb1cd7dc4a3a1
mizhuo <wulibin0809@gmail.com>
2022-07-13 21:35:45 +0800

    add arthas-demo

---
+++ StudyDemo/arthas-demo/src/test/java/site/mizhuo/arthasdemo/ArthasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.mizhuo.arthasdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
